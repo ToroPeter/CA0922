3ab1f3a9cadd245f914d071e3a408b75ce547c97
ToroPeter <zeen17ben@gmail.com>
2020-09-22 10:31:42 +0200

    rndColor

--- CA0922/Program.cs
+++ CA0922/Program.cs
@@ -8,10 +8,19 @@ namespace CA0922
 {
     class Program
     {
+        static Random rnd = new Random();
         static void Main()
         {
             Console.ForegroundColor = ConsoleColor.Blue;
             Console.WriteLine("Saját név");
+            for (int i = 0; i < 100; i++)
+            {
+                Console.ForegroundColor = (ConsoleColor)rnd.Next(1, 16);
+                Console.SetCursorPosition(
+                    rnd.Next(Console.WindowWidth),
+                    rnd.Next(Console.WindowHeight));
+                Console.Write("*");
+            }
             Console.ReadKey(true);
         }
     }
