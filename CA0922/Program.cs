ee6545d4dccdf9fc4efd0a03034d0362a04fcf2e
ToroPeter <zeen17ben@gmail.com>
2020-09-22 10:20:31 +0200

    Add project files.

---
+++ CA0922/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA0922
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Saját név");
+            Console.WriteLine("klnnklsnfk");
+            
+
+            Console.ReadKey(true);
+        }
+    }
+}
